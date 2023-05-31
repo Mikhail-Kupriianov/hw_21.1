dead11d5f665a976021f5816bb65137e6a1d69f2
Mkupr <mkupr@outlook.com>
2023-05-31 18:32:53 +0300

    init new Django project

---
+++ catalog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'catalog/home.html')
+
+
+def index2(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({phone}): {message}')
+    return render(request, 'catalog/contacts.html')
