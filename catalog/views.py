3f77eb2293f2ded55f5dc474665792a6b2d7de22
Mkupr <mkupr@outlook.com>
2023-06-06 00:20:02 +0300

    edit view.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from catalog.models import Product
+
+
+class ContactListView(ListView):
+    paginate_by = 3
+    model = Product
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'catalog/home.html')
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/home.html', context, )
 
 
-def index2(request):
+def contact(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'You have new message from {name}({phone}): {message}')
-    return render(request, 'catalog/contacts.html')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contacts.html', context)
